ff0bbd8f75f456fabf0ffbf2927bcf1dc1fea902
Adam La Morre <alamorre@gmail.com>
2021-09-29 08:15:59 -0400

    chat engine APIs working

--- src/SupportEngine/SupportWindow/index.js
+++ src/SupportEngine/SupportWindow/index.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react"
 
 import { styles } from "../styles";
 
 import EmailForm from "./EmailForm";
 
 const SupportWindow = props => {
+    const [user, setUser] = useState(null)
+    const [chat, setChat] = useState(null)
+
     return (
         <div 
             className='transition-5'
@@ -13,7 +16,16 @@ const SupportWindow = props => {
                 ...{ opacity: props.visible ? '1' : '0' }
             }}
         >
-            <EmailForm />
+            <EmailForm 
+                visible={user === null || chat === null}
+                setUser={user => setUser(user)} 
+                setChat={chat => setChat(chat)} 
+            />
+
+            {
+                user !== null && chat !== null &&
+                <div>Chat Engine</div>
+            }
         </div>
     )
 }
